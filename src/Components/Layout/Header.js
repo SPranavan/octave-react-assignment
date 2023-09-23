6d25d2eaafa8ae217dacfc8ba8133ce074e36a56
Pranavan <spranavan008@gmail.com>
2023-09-23 10:18:00 +0530

    Add Favicon and Logo

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Styles/Header.css';
 import '../../App.css';
+import logo from '../Images/logo.png'
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="header-container">
+            <div className="logo-container">
+              <Link to="/" className="navbar-brand">
+                <img src={logo} alt="logo" className="logo" style={{height:"60px"}}/>
+              </Link>
+            </div>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="header-nav-item nav-item">
